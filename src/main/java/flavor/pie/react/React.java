6a4bd30181ccbe91458d51dcdd02057dcec3cd5b
Adam Spofford <pieflavor.mc@gmail.com>
2016-06-23 23:46:21 -0700

    Version bump to 1.2.3

--- src/main/java/flavor/pie/react/React.java
+++ src/main/java/flavor/pie/react/React.java
@@ -35,7 +35,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
-@Plugin(id="react",name="React",description="A little game to be played in chat.",authors="pie_flavor",version="1.2.2")
+@Plugin(id="react",name="React",description="A little game to be played in chat.",authors="pie_flavor",version="1.2.3")
 public class React {
     @Inject
     Game game;
